1ce3dec24eb04953a28687f225874f248d4d7c53
Natanael <nzegarraarteaga@gmail.com>
2018-02-17 04:23:31 -0500

    Se ga modificado la variablde copy right y se ha agregado el nombre del proyecto

--- src/main/java/org/vass/prueba/JAXRSMessage/util/Constants.java
+++ src/main/java/org/vass/prueba/JAXRSMessage/util/Constants.java
@@ -4,5 +4,6 @@ public class Constants{
 	public static final String ADMIN_NAME = "Natanael";
 	public static final String ADMIN_APELLIDO_PATERNO = "Zegarra";
 	public static final String ADMIN_APELLIDO_MATERNO = "Arteaga";
-	public static final String COPY_RIGHT = "Java Brains";
+	public static final String COPY_RIGHT_AUTHOR = "Java Brains";
+	public static final String COPY_RIGHT_NAME_PROJECT = "RESTful with JAXRS - Jersey and Maven";
 }
