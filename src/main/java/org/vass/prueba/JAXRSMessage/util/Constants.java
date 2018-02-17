1d80d1bf542f0f02c3428b934c3014d12b0d45d1
Natanael <nzegarraarteaga@gmail.com>
2018-02-17 02:55:57 -0500

    Se ha agregado las constantes apellido paterno, materno y una dependecnia log4j

--- src/main/java/org/vass/prueba/JAXRSMessage/util/Constants.java
+++ src/main/java/org/vass/prueba/JAXRSMessage/util/Constants.java
@@ -2,4 +2,6 @@ package org.vass.prueba.JAXRSMessage.util;
 
 public class Constants{
 	public static final String ADMIN_NAME = "Natanael";
+	public static final String ADMIN_APELLIDO_PATERNO = "Zegarra";
+	public static final String ADMIN_APELLIDO_MATERNO = "Arteaga";
 }
