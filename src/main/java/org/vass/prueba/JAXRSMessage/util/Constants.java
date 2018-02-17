ac6578faea13af850a7dce3581b891d6874dff11
Natanael <nzegarraarteaga@gmail.com>
2018-02-17 04:13:18 -0500

    Se ha agragado en las constantes el nombre del autor

--- src/main/java/org/vass/prueba/JAXRSMessage/util/Constants.java
+++ src/main/java/org/vass/prueba/JAXRSMessage/util/Constants.java
@@ -4,4 +4,5 @@ public class Constants{
 	public static final String ADMIN_NAME = "Natanael";
 	public static final String ADMIN_APELLIDO_PATERNO = "Zegarra";
 	public static final String ADMIN_APELLIDO_MATERNO = "Arteaga";
+	public static final String COPY_RIGHT = "Java Brains";
 }
